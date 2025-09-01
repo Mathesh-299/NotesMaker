ebb6b869fc91518e0cbc755e94eb3d71e6938dc4
Mathesh-299 <postbox2992005@gmail.com>
2025-09-01 22:55:22 +0530

    Update the basedUrl

--- notefend/src/API/api.js
+++ notefend/src/API/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://note-craft-dt8t.onrender.com/api",
 });
 
 export default API;
